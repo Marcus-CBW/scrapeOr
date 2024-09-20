85f844e24cb5851c16916bcc25acfa954767b8ef
Marcus Link <marcus.link@cbw-onlineschulung.de>
2024-09-20 23:12:58 +0200

    Vorbereitungsphase

--- jobportale/jobportale/items.py
+++ jobportale/jobportale/items.py
@@ -9,4 +9,12 @@ import scrapy
 class JobportaleItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    job_titel = scrapy.Field()
+    firma = scrapy.Field()
+    ort = scrapy.Field()
+    meta_daten = scrapy.Field()
+    url_portal = scrapy.Field()
+    url_firma = scrapy.Field()
+    geschaltet = scrapy.Field()
+    
+#https://de.indeed.com/jobs?q=Fachinformatiker+Anwendungsentwicklung&l=Frankfurt+am+Main&from=searchOnHP
--- src/cookie_export.py
+++ src/cookie_export.py
@@ -4,26 +4,43 @@ from secret import firefox_profile_path
 
 cookies_db = os.path.join(firefox_profile_path, "cookies.sqlite")
 
-# Verbindung zur SQLite-Datenbank herstellen
-connection = sqlite3.connect(cookies_db)
-cursor = connection.cursor()
 
-# Alle Cookies abfragen, die "indeed" im Hostnamen enthalten
-cursor.execute("SELECT host, name, value FROM moz_cookies WHERE host LIKE '%indeed%'")
+try:
+    # Verbindung zur SQLite-Datenbank herstellen
+    verbindung = sqlite3.connect(cookies_db)
+    cursor = verbindung.cursor()
 
-# Ergebnisse in einer Variablen speichern
-cookies = cursor.fetchall()
+    # Alle Cookies abfragen, die "indeed" im Hostnamen enthalten
+    cursor.execute("SELECT host, name, value FROM moz_cookies WHERE host LIKE '%indeed%'")
 
-# Ergebnisse in eine Textdatei speichern
-with open("cookies_output.txt", "w+", encoding="utf-8") as file:
+    # Ergebnisse in einer Variablen speichern
+    cookies = cursor.fetchall()
+
+    # Ergebnisse in eine Textdatei speichern
+    datei = open("cookies_secret.py", "w+", encoding="utf-8")
+
+    datei.write("'''\n") 
     for host, name, value in cookies:
-        file.write(f"Host: {host}, Cookie-Name: {name}, Cookie-Wert: {value}\n")
+        #print(f"Host: {host}, Cookie-Name: {name}, Cookie-Wert: {value}")
+        datei.write(f"Host: {host}, Cookie-Name: {name}, Cookie-Wert: {value}\n")
+    datei.write("'''") 
 
-# # Ergebnisse in der Konsole ausgeben
-# for host, name, value in cookies:
-#     print(f"Host: {host}, Cookie-Name: {name}, Cookie-Wert: {value}")
+except sqlite3.DatabaseError as e:
+    print(f"Datenbankfehler: {e}")
 
-# # Verbindung schließen
-# connection.close()
+except OSError as e:
+    print(f"Dateifehler: {e}")
 
-print("Datei 'cookies_output.txt' wurde erfolgreich erstellt.")
+except Exception as e:
+    print(f"Ein Fehler ist aufgetreten: {e}")
+
+else:
+    print("Vorgang war erfolgreich.")
+
+finally:
+    # Sicherstellen, dass die Verbindung geschlossen wird, falls sie geöffnet wurde
+    #print(dir())
+    if 'verbindung' in locals():
+        verbindung.close()
+    if 'datei' in locals():
+        datei.close()
